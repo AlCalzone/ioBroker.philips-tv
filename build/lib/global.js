4461e339ee34b2a5eccc97acba399238c0040d1f
dominic.griesel <d.griesel@gmx.net>
2019-05-03 14:14:39 +0200

    Switch to new testing, enable compact mode

--- build/lib/global.js
+++ build/lib/global.js
@@ -14,8 +14,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
         while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
                 case 0: case 1: t = op; break;
                 case 4: _.label++; return { value: op[1], done: false };
@@ -113,6 +113,7 @@ var Global = /** @class */ (function () {
                 return __generator(this, function (_a) {
                     switch (_a.label) {
                         case 0: return [4 /*yield*/, ret.$setObject(id, {
+                                _id: id,
                                 type: "state",
                                 common: {
                                     name: id,
@@ -171,7 +172,7 @@ var Global = /** @class */ (function () {
                 if (typeof repl === "string") {
                     message = message.replace(regex, repl);
                 }
-                else {
+                else { // a bit verbose, but TS doesn't get the overload thingy here
                     message = message.replace(regex, repl);
                 }
             }
---
+++ build/main.d.ts
@@ -0,0 +1,8 @@
+declare global {
+    namespace ioBroker {
+        interface AdapterConfig {
+            host: string;
+        }
+    }
+}
+export {};
